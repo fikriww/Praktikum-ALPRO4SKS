279b04240002c5c50b7d1672e8d4716822930d64
fikriww <91867817+fikriww@users.noreply.github.com>
2024-06-04 19:04:22 +0700

    Delete pert3 directory

--- pert3/BilSempN.c
+++
@@ -1,6 +0,0 @@
-/*
-Nama    		: Dzu Sunan Muhammad
-NIM     		: 24060122120034
-Nama Program 	: BilPSempN.c
-Deskripsi 		: Mengidentifikasi dan menampilkan pada layar pada sampai deret berapakah inputan N
-*/
